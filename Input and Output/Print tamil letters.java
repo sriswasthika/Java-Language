2c1ea17ce08981401d6cdd08f2bbb2ee83e4b389
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2025-02-04 23:00:49 +0530

    Create Print tamil letters.java

---
+++ Input and Output/Print tamil letters.java
@@ -0,0 +1,11 @@
+public class Main
+{
+  public static void main (String[]args)
+  {
+    System.out.println ("\u0bb5" + " " + "\u0ba3" + "  " + "\u0b95" +
+			"\u0b82" + " " + "\u0b95" + " " + "\u0bae" +
+			"\u0b82");
+  }
+}
+
+//Output:Good morning in tamil language
